933296a0c686f2661c6f8dbcde61129b6d465f1e
Jessica Tallon <jessica@megworld.co.uk>
2014-04-07 17:14:09 -0500

    Add sending and receiving updates to pump.io plugin

--- waterworks/plugins/pumpio.py
+++ waterworks/plugins/pumpio.py
@@ -14,12 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with Waterworks.  If not, see <http://www.gnu.org/licenses/>.
 
-
-import pypump
+try:
+    import pypump
+except ImportError:
+    pass
 
 from waterworks.protoprotocol import ProtoProtocol
-
-
 
 class PumpProtocol(ProtoProtocol):
     """ Protocol for interacting with the pump.io service """
@@ -31,6 +31,7 @@ class PumpProtocol(ProtoProtocol):
         self.config = config
 
     def connect(self):
+        """ Setup PyPump client and instance """
         if "client" in storage:
             self.client = pypump.Client(
                 webfinger=self.config["webfigner"],
@@ -52,8 +53,45 @@ class PumpProtocol(ProtoProtocol):
         )
 
     def __verifier_callback(self, url):
+        """
+        Handles OAuth out of band authentication
 
+        We need the user to click on the URL, follow the instructions
+        and then provide the verifier token via stdin. A possible future
+        consideration could be to send a request to any connected clients
+        to ask the user to open this URL.
+        """
         print url
         verifier = raw_input("Verifier: ").strip(" ")
         return verifier
 
+    def send_msg(self, msg, targets=None):
+        if targets is None:
+            # If it's got no specific targets send it to everyone (i.e. public)
+            targets = [self.pump.Lists.Public]
+
+        note = self.pump.Note(msg)
+        note.to = targets
+        note.send()
+
+    def get_updates(self):
+        if self.previous_message is None:
+            # We don't have any preverious messages so just give back the last 20
+            notes = self.pump.Inbox[:20]
+            self.previous_message = notes[0].id
+            return notes
+
+        # really we should have a way of getting the messages since another message
+        # but until we're able to easily do that we can force it to hand back whatever
+        # the last messages where
+        #
+        # Related bug: https://github.com/xray7224/PyPump/issues/100
+        self.previous_message = None
+        return self.get_update()
+
+    def is_available(self):
+        try:
+            import pypump
+            return True
+        except:
+            return False
